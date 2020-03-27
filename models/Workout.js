1a808078aae64b071d72b83b98ec19cd3a4cdebb
Nelson Ezeume <nelsonezeume@yahoo.com>
2020-03-26 23:33:06 -0400

    corrected syntax errors

--- models/Workout.js
+++ models/Workout.js
@@ -3,38 +3,38 @@ const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema(
     {
-        day:{
-        type: Date,
-        default: () => new Date()
-    },
-    exercises: [
-        {
-           type:{
-               type:String,
-               required:true
-           },
-           name:{
-               type:String,
-               required:true
-           },
-           duration:{
-               type:Number,
-               required:true
-           },
-           weight:{
-               type:Number,
-            
-           },
-           reps:{
-               type:Number
-           },
-           sets:{
-               type:Number
-           }
-        }
-    ]
-   
-});
+        day: {
+            type: Date,
+            default: () => new Date()
+        },
+        exercises: [
+            {
+                type: {
+                    type: String,
+                    required: true
+                },
+                name: {
+                    type: String,
+                    required: true
+                },
+                duration: {
+                    type: Number,
+                    required: true
+                },
+                weight: {
+                    type: Number,
+
+                },
+                reps: {
+                    type: Number
+                },
+                sets: {
+                    type: Number
+                }
+            }
+        ]
+
+    });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
--- server.js
+++ server.js
@@ -24,66 +24,54 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { use
 // Update just one note by an id
 app.put("/api/workouts/:id", (req, res) => {
     db.Workout.findByIdAndUpdate(
-      req.params.id,
-      { $push: { exercises: req.body } },
-      // "runValidators" will ensure new exercises meet our schema requirements
-      { new: true, runValidators: true }
+        req.params.id,
+        { $push: { exercises: req.body } },
+        // "runValidators" will ensure new exercises meet our schema requirements
+        { new: true, runValidators: true }
     )
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
-
-  app.post("/api/workouts", function(req, res) {
-      db.Workout.create({})
-      .then(dbWorkout=>{
-          res.json(dbWorkout);
+        .then(dbWorkout => {
+            res.json(dbWorkout);
         })
-      .catch(err=>{
-          res.json(err);
+        .catch(err => {
+            res.json(err);
         });
-    // When searching by an id, the id needs to be passed 
-    // console.log(req.body);
-    // res.json
-  });
+});
 
-//   app.post("/submit", ({ body }, res) => {
-//     db.Note.create(body)
-//       .then(({ _id }) => db.User.findOneAndUpdate({}, { $push: { notes: _id } }, { new: true }))
-//       .then(dbUser => {
-//         res.json(dbUser);
-//       })
-//       .catch(err => {
-//         res.json(err);
-//       });
-//   });
+app.post("/api/workouts", function (req, res) {
+    db.Workout.create({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+
+});
+
 
 
 app.get("/api/workouts", (req, res) => {
     db.Workout.find({})
-      //.sort({ date: -1 })
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
-  });
+        //.sort({ date: -1 })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
 
-  app.get("/api/workouts/range", (req, res) => {
+app.get("/api/workouts/range", (req, res) => {
     db.Workout.find({})
-    .limit(7)
-      //.sort({ date: -1 })
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
-  });
+        .limit(7)
+        //.sort({ date: -1 })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public/index.html"));
